d75a0d23e2dea67e5e883abf175d4b8538b151b8
JailsonSantos <jailson.ads007@gmail.com>
2023-05-23 10:16:24 -0300

    Feat: Components Header and Hero-Section

---
+++ app/components/header/nav_item.tsx
@@ -0,0 +1,26 @@
+"use client"
+
+import Link from "next/link"
+import { usePathname } from "next/navigation"
+import { classNamePersonalty } from "@/app/lib/utils"
+
+type NavItemProps = {
+  label: string
+  href: string
+}
+
+export const NavItem = ({ label, href }: NavItemProps) => {
+  const pathname = usePathname();
+
+  const isActive = pathname === href;
+
+  return (
+    <Link href={href} className={classNamePersonalty(
+      "text-gray-400 flex items-center gap-2 font-medium font-mono",
+      isActive && "text-gray-50"
+    )}>
+      <span className="text-emerald-400">#</span>
+      {label}
+    </Link>
+  )
+}
